dea54ccf3952bb43a042e2a499941592bf9849d8
saremi <saremi.hadi1992@gmail.com>
2024-11-20 12:01:58 +0330

    BaseHttpService

---
+++ ShopCenter.MVC/Services/Base/BaseHttpService.cs
@@ -0,0 +1,42 @@
+﻿using ShopCenter.MVC.Contracts;
+using System.Net.Http.Headers;
+
+namespace ShopCenter.MVC.Services.Base
+{
+    public class BaseHttpService
+    {
+        private readonly IClient _client;
+        private readonly ILocalStorageService _localStorage;
+
+        public BaseHttpService(IClient client,ILocalStorageService localStorage)
+        {
+            _client = client;
+            _localStorage = localStorage;
+        }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException exception)
+        {
+            if (exception.StatusCode == 400)
+            {
+                return new Response<Guid>() { Message = "Validation errors have occured.", ValidationErrors = exception.Response, Success = false };
+            }
+            else if (exception.StatusCode == 404)
+            {
+                return new Response<Guid>() { Message = "Not Found ...", Success = false };
+            }
+            else
+            {
+                return new Response<Guid>() { Message = "Somthing went wrong,try again ... ", Success = false };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorage.Exists("token"))
+            {
+                _client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _localStorage.GetStorageValue<string>("token"));
+            }
+        }
+    }
+}
