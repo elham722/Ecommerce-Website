69c62fbca2d0269c6504297114fcafaf58b16bbf
Elham <elham.heydari722@gmail.com>
2024-11-22 09:49:04 +0330

    repo

---
+++ ShopCenter.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using ShopCenter.Application.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCenter.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+        Task<RegistrationResponse> Register(RegisterationRequest request);
+    }
+}
