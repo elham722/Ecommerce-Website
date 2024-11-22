1f2e1e3f720f113e3816a41afb46a8ff17624b33
Elham <elham.heydari722@gmail.com>
2024-11-22 17:04:31 +0330

    87

--- ShopCenter.Api/Program.cs
+++ ShopCenter.Api/Program.cs
@@ -2,6 +2,7 @@ using ShopCenter.Infrastructure;
 using ShopCenter.Application;
 using ShopCenter.Persistence;
 using ShopCenter.Identity;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +15,8 @@ builder.Services.ConfigurePersistenceServices(builder.Configuration);
 builder.Services.ConfigureIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//builder.Services.AddSwaggerGen();
+AddSwagger(builder.Services);
 
 builder.Services.AddCors(o =>
 {
@@ -44,3 +45,44 @@ app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
+
+void AddSwagger(IServiceCollection services)
+{
+    services.AddSwaggerGen(o =>
+    {
+        o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 1234sddsw'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        o.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference()
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+
+        o.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "ShopCenter Api"
+        });
+    });
+}
